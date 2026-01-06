560289c556bdf18510f9147787cfd22a6c48cfc0
Shubham Verma <91446735+Shubh9907@users.noreply.github.com>
2026-01-07 00:38:22 +0530

    changes for tomcat

--- src/main/java/com/bridgelabz/fundoo_notes/FundooNotesApplication.java
+++ src/main/java/com/bridgelabz/fundoo_notes/FundooNotesApplication.java
@@ -2,12 +2,18 @@ package com.bridgelabz.fundoo_notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class FundooNotesApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FundooNotesApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FundooNotesApplication.class, args);
